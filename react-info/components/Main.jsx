000a39d5e627b98a05ff595cd422d62b115db662
GalkinAllan <galkinallan@gmail.com>
2022-10-26 18:53:01 +0300

    firstProject

---
+++ react-info/components/Main.jsx
@@ -0,0 +1,18 @@
+
+function Main() {
+    return (
+    <div className="main">
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+
+    )
+}
+
+export default Main
---
+++ react-info/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar() {
+    return (
+    <div className="nav">
+        <img src="./images/logo.png" alt="" />
+        <h4>React Course - Project 1</h4>
+    </div>
+
+    )
+}
+
+export default Navbar
